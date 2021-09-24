26ddee8e65a7139dd4d7f6c80acbe7272066ddbb
João Paiva <joao.vitor.vbp@gmail.com>
2021-09-24 01:39:57 -0300

    Anotacoes, preparando para implementar a propagacao na rede

--- src/main/java/MLP/MLP.java
+++ src/main/java/MLP/MLP.java
@@ -9,12 +9,42 @@ public class MLP {
     public Camada camadaSaida;
     public double taxaDeAprendizado;
 
-    public MLP(int neuroniosCamadaOculta, double taxaDeAprendizado){
+    public MLP(int neuroniosCamadaOculta, double taxaDeAprendizado) {
         this.taxaDeAprendizado = taxaDeAprendizado;
-        camadaOculta = new Camada(neuroniosCamadaOculta,TAM_ENTRADA);
+        camadaOculta = new Camada(neuroniosCamadaOculta, TAM_ENTRADA);
         camadaSaida = new Camada(10, camadaOculta.tamanho);
     }
 
+    // INSTRUCOES PARA A ETAPA DE PROPAGACAO
+//    ◼ Passo 1.1) Dado o exemplo E como entrada da
+//    rede, determine:
+//            – as saídas dos neurônios ocultos (que serão os
+//            sinais de entrada para as unidades de saída).
+//            – as saídas dos neurônios de saída (que indicam a
+//            estimativa de classe para o exemplo E).
+//    Passo 1.2) Registre os valores desejado e obtido para o exemplo E.
+//            – considere ti
+//            (E) o valor desejado para a unidade de saída i, para o
+//    exemplo E.
+//            – considere oi
+//            (E) o valor obtido para a unidade de saída i, para o
+//    exemplo E.
+//            ◼ Para tarefas de classificação, por exemplo:
+//            – Cada ti
+//            (E) será igual a 0, exceto para um único tj
+//            (E), que será igual
+//    a 1.
+//            – Mas, oi
+//            (E) será, na verdade, um valor real.
+//            ◼ Os valores de saída dos neurônios ocultos hi
+//    também devem ser
+//    registrados.
 
+    public double forwardPropagation(){
+        double output = 0;
+        //Passar os valores pela rede toda, ate receber um output
+            //
+        return output;
+    }
 
 }
