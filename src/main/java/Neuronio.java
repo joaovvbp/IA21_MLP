ab3320e2d4acdb5bab069328c0fa04d592d38d46
João Paiva <joao.vitor.vbp@gmail.com>
2021-09-23 22:00:02 -0300

    Implementacao de Inicilizacao e Normalizacao de pesos

--- src/main/java/Neuronio.java
+++ src/main/java/Neuronio.java
@@ -1,12 +1,27 @@
+import java.util.Random;
+
 public class Neuronio {
-    public void feedForward(){
+    Random random = new Random();
+    protected double[] pesos;
+
+    public Neuronio(int pesosRecebidos) {
+        pesos = new double[pesosRecebidos];
     }
 
-    public double normalizaPesos(){
-        return 0.0;
+    public void inicializaPesos() {
+        for (int i = 0; i < pesos.length; i++) {
+            pesos[i] = random.nextDouble();
+        }
     }
 
-    public double somaPonderada(){
-        return 0.0;
+    public void normalizaPesos() {
+        double somapesos = 0;
+        for (double d : pesos) {
+            somapesos += d;
+        }
+        for (int i = 0; i < pesos.length; i++) {
+            pesos[i] = pesos[i]/somapesos;
+        }
     }
+
 }
