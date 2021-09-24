a26dece32811c5ddd14d6d92f7725b0f33ab6b2b
João Paiva <joao.vitor.vbp@gmail.com>
2021-09-23 23:48:29 -0300

    Algumas refatoracoes e implementacao de MLP

---
+++ src/main/java/MLP/MLP.java
@@ -0,0 +1,13 @@
+package MLP;
+
+public class MLP {
+    final int MIN_PESO = -1;
+    final int MAX_PESO = 1;
+//  A rede solicitada no enunciado possui apenas uma camada oculta.
+    private double taxaDeAprendizado = 0.1;
+    private Camada[] camadas;
+
+    public MLP(int[] neuroniosPorCamadas, double taxaDeAprendizado){
+
+    }
+}
