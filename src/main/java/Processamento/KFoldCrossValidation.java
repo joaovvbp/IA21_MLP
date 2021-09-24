b69188c6a21b8276f6606ea354749849f305cb29
gdesouza-git <gustavoo.desouza@outlook.com>
2021-09-24 00:19:29 -0300

    Desenvolvimento KFoldCrossValidation

--- src/main/java/Processamento/KFoldCrossValidation.java
+++ src/main/java/Processamento/KFoldCrossValidation.java
@@ -1,3 +1,5 @@
+package Processamento;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -5,16 +7,30 @@ public class KFoldCrossValidation {
 
     static ArrayList<ArrayList<Exemplo>> folds = new ArrayList<>();
 
+    public static void kFoldCrossValidation(){
+        embaralharDados();
+        dividir();
+        exibirTamanhos();
+    }
+
     public static void embaralharDados(){
         Collections.shuffle(ProcessamentoDeArquivo.entradas);
     }
-    
+
     public static void dividir(){
         for (Exemplo vetor : ProcessamentoDeArquivo.entradas) {
             ArrayList<Exemplo> fold = new ArrayList<>();
-            for (int i = 0; i < ProcessamentoDeArquivo.entradas.size()/10; i++) {
-                
-            }
+            for (int i = 0; i < ProcessamentoDeArquivo.entradas.size()/10; i++)
+                fold.add(ProcessamentoDeArquivo.entradas.get(i));
+            fold.clear();
         }
     }
+
+    public static void exibirTamanhos(){
+        for (int i = 0; i < folds.size(); i++) {
+            System.out.println("fold ("+i+"): "+folds.get(i).size());
+        }
+
+    }
+
 }
